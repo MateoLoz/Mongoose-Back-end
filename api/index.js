7813133c61ad3ba8de3b0453a88a743ec280606b
Mateo Lozano <119082981+MateoLoz@users.noreply.github.com>
2024-09-30 16:22:53 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,28 +8,11 @@ const port = 3336;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors());
+app.use(cors({origin: 'http://localhost:5173'}));
 app.use(bodyparser.urlencoded({extended: true}));
 const {mongooseconection} = require('../controller/mongocontroller');
 app.use(require('../router/router'));
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 mongooseconection();
 
