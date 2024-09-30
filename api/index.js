a7373aadc092a59164d904b6a3f707affce8c022
Mateo Lozano <119082981+MateoLoz@users.noreply.github.com>
2024-09-30 16:19:43 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(require('../router/router'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
