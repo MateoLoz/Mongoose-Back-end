1437aa5e60f21374334890dc0aafc764d31e5bd1
Mateo Lozano <119082981+MateoLoz@users.noreply.github.com>
2024-09-30 16:37:19 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,10 +3,13 @@ const app = express();
 
 const bodyparser = require('body-parser');
 const port = 3336;
+const cors = require('cors);
 
+const corsOptions = {
+    origin:'http://localhost:5173'
+};
 
-
-
+app.use(cors(corsOptions);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
