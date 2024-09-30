c7136c8c2d395bd39cb47ba6b8d2b4275820b6a2
Mateo Lozano <119082981+MateoLoz@users.noreply.github.com>
2024-09-30 16:40:34 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const corsOptions = {
     origin:'http://localhost:5173'
 };
 
-app.use(cors(corsOptions);
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
