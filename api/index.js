1979d0be2ad5ddeba734a6a7487cdb6d7b06c57f
Mateo Lozano <119082981+MateoLoz@users.noreply.github.com>
2024-09-30 15:29:16 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,13 +16,6 @@ app.use(require('../router/router'));
 mongooseconection();
 
 
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
 app.get('/', (req,res)=>{
     res.send('Mongo serverside!');
